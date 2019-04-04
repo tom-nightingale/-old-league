7a01138a0c8bc884f7f6158601803a40ae99e53d
Tom Nightingale <thomas.nightingale@gmail.com>
2019-04-04 21:28:57 +0100

    Adding ServicesPagePreview to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import ServicesPagePreview from './preview-templates/ServicesPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('about', ServicesPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
