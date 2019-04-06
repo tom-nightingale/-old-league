19c11fb92128550be22acdefe16285d631d1d29e
Tom Nightingale <thomas.nightingale@gmail.com>
2019-04-06 18:31:03 +0100

    Adding in assets for logo Playing aorund with tyopgraphy

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import logo from "../img/logo.svg";
+import logo from "../img/logo-text.svg";
 
 const Logo = class extends React.Component {
     render() {
         return (
             <Link to="/" className="logo" title="Logo">
-              <img src={logo} alt="Kaldi" />
+              <img src={logo} alt="League Digital" />
             </Link>
         )
     }
