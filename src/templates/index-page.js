f6114308f33fe695f62a8eeac8a017f2025cdc99
Tom Nightingale <thomas.nightingale@gmail.com>
2019-04-16 20:50:58 +0100

    Updating hero to use CMS

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -18,8 +18,8 @@ export const IndexPageTemplate = ({
       <div className="hero">
 
           <div className="hero__section">
-              <p className="hero__headline">Web design & <br />digital marketing.</p>
-              <p className="hero__subline">We design and build websites that generate more leads for your business.</p>
+              <p className="hero__headline">{hero.heading}</p>
+              <p className="hero__subline">{hero.subeading}</p>
               <Link className="hero__button" to="/services">See how we can help <Icon name="icon-right-open-mini"/></Link>
           </div>
 
