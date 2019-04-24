2eb1a91c356550e040153d6d850694b86c87be08
Tom Nightingale <thomas.nightingale@gmail.com>
2019-04-24 21:56:42 +0100

    Services updated - need to add in the icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
-import Icon from './NewIcon';
 
 const FeatureGrid = ({ heading, description, gridItems }) => (
   <div className="services">
@@ -24,7 +23,7 @@ const FeatureGrid = ({ heading, description, gridItems }) => (
               !!item.image.childImageSharp ? item.image.childImageSharp.fluid.src : item.image
             })`,
           }}>
-              <Icon name={item.icon}/>
+              {item.icon}
               <p className="services-single__heading">{item.heading}</p>
               <p className="services-single__text">{item.text}</p>
           </Link>
